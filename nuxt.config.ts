5b789743a61dc31220fe4292030d8b95cc7892cf
Yuting Hsiao <and535014@gmail.com>
2023-12-13 15:57:29 +0800

    Install primeicons package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ["primeicons/primeicons.css"],
   modules: [
     "@nuxtjs/eslint-module",
     "@nuxtjs/i18n",
